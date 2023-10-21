89cfb1c74856697c93d7c63e0d4b1e09de3bab43
patrick <patrick@GTbank>
2023-10-21 14:07:37 +0300

    update on graphing

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import calendar
+import plotly_express as px 
+
+st.set_page_config(
+    page_title="GAPS Dasbboard View",
+    page_icon="🚀",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 
 st.sidebar.title('GTBANK (Uganda)')
@@ -73,7 +87,11 @@ def failedTransactions(processed_data):
 
     return failed_data
 
-
+def interactive_plot(dataframe):
+    x_axis_val = st.selectbox('Select X - Axis Value', options = dataframe.columns)
+    y_axis_val = st.selectbox('Select Y - Axis Value', options = dataframe.columns)
+    plot = px.scatter(dataframe , x=x_axis_val, y=y_axis_val)
+    st.plotly_chart(plot)
 
 if csv_file is not None:
     data_load_state = st.sidebar.text('Loading data...')
@@ -84,19 +102,43 @@ if csv_file is not None:
     successful_data_v = successfulTransaction(processed_data_v)
     failed_data_v = failedTransactions(processed_data_v)
 
+    col1, col2, col3, col4 = st.columns((2,1,1,1))
+    
+    
+    with col1:
+        st.subheader("Total Transaction Count for Each Month")
+        successful_data_v['YearMonth'] = successful_data_v[PAYMENT_DATE_COLUMN].dt.month
+        monthly_counts = successful_data_v['YearMonth'].value_counts().sort_index()
+
+
+    with col2:
+        st.subheader("Top Day")
+
+    with col3:
+        st.subheader("Top Day")
+
+    with col4:
+        st.subheader("Top Day")
+
+    tab1, tab2, tab3, tab4 = st.tabs(["Raw Data", "Processed", "Successful Transactions", "Failed Transactions"])
+
     st.sidebar.text("Gaps Data Filter")
     if st.sidebar.checkbox('Raw data'):
-        st.subheader(f'Raw data {len(data)}')
-        st.write(data)
+        with tab1:
+            st.subheader(f'Raw data ({len(data)})')
+            st.write(data)
 
     if st.sidebar.checkbox(f'({len(processed_data_v)}) Processed data'):
-        st.subheader('Processed data')
-        st.write(processed_data_v)
+        with tab2:
+            st.subheader(f'Processed data ({len(processed_data_v)})')
+            st.write(processed_data_v)
 
     if st.sidebar.checkbox(f'({len(successful_data_v)}) Successful Transactions'):
-        st.subheader('Successful Transactions')
-        st.write(successful_data_v)
+        with tab3:
+            st.subheader(f'Successful Transactions ({len(successful_data_v)})')
+            st.write(successful_data_v)
 
     if st.sidebar.checkbox(f'({len(failed_data_v)}) Failed Transactions'):
-            st.subheader('Failed Transactions ')
+        with tab4:
+            st.subheader(f'Failed Transactions ({len(failed_data_v)}) ')
             st.write(failed_data_v)
