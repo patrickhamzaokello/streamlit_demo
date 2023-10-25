bc09d01a212aea76e38ed6c4353b328cc901a51a
patrick <patrick@GTbank>
2023-10-25 14:53:17 +0300

    fie on dasshabord

--- dashboard.py
+++ dashboard.py
@@ -5,24 +5,29 @@ import calendar
 import plotly_express as px 
 import humanize
 
+# Load and apply custom CSS
+def load_custom_css():
+    with open("assets/custom.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
 st.set_page_config(
     page_title="GAPS Dasbboard View",
     page_icon="🚀",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
 )
+
+# Load custom CSS
+load_custom_css()
 
 
 st.sidebar.title('GTBANK (Uganda)')
 st.sidebar.subheader('GAPS Dashboard')
 st.sidebar.divider()
-PAYMENT_DATE_COLUMN = "PaymentDate"
 PAID_DATE_COLUMN = "PaidDate"
+PAYMENT_DATE_COLUMN = "PaymentDate"
 ordered_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 
@@ -32,8 +37,8 @@ st.sidebar.divider()
 @st.cache_data
 def load_data(csv_file_data):
     data = pd.read_csv(csv_file_data)
-    data[PAYMENT_DATE_COLUMN] = pd.to_datetime(data[PAYMENT_DATE_COLUMN], format='mixed')
     data[PAID_DATE_COLUMN] = pd.to_datetime(data[PAID_DATE_COLUMN], format='mixed')
+    data[PAYMENT_DATE_COLUMN] = pd.to_datetime(data[PAYMENT_DATE_COLUMN], format='ISO8601')
     return data
 
 @st.cache_data
@@ -68,30 +73,35 @@ def processed_data(raw_data):
         'ProcessType',
         'ProcessType_Description',
         'PaymentDeclined',
+        'PaymentFlag',
         'Remarks'
     ]
     
     processed_data = raw_data[selected_columns]
+
+    # Remove records with PaidDate of null
+    # processed_data = processed_data.dropna(subset=[PAID_DATE_COLUMN])
+
     return processed_data
 
 @st.cache_data
 def successfulTransaction(processed_data):
-    successful_data = processed_data[
-    (processed_data['PaymentDeclined'] == 0) &
-    (processed_data['ProcessFlag'].isin(['A']))]
+    successful_data = processed_data[(processed_data['PaymentDeclined'] == 0) & (processed_data['PaymentFlag'] == 1) & (processed_data['ProcessFlag'].isin(['A']))]
 
     return successful_data
 
 
 @st.cache_data
-def failedTransactions(processed_data):
-    failed_data = processed_data[
-    ~(
-        (processed_data['PaymentDeclined'] == 0) &
-        (processed_data['ProcessFlag'].isin(['A'])) 
-    )]
+def notDeclined(processed_data):
+    unDeclined_data = processed_data[(processed_data['PaymentDeclined'] == 0)]
 
-    return failed_data
+    return unDeclined_data
+
+@st.cache_data
+def declinedTransactions(processed_data):
+    declined_data = processed_data[(processed_data['PaymentDeclined'] == 1)]
+
+    return declined_data
 
 @st.cache_data
 def Quarterly_Count_Pie_chart(quarterly_summary):    
@@ -103,13 +113,14 @@ def Quarterly_Count_Pie_chart(quarterly_summary):
     fig.update_layout(
         title=f'Percent of Transaction Total Count by Quarter',
         xaxis_title='Month',
-        yaxis_title='Count'
+        yaxis_title='Count',
+        legend=dict(orientation='v', y=1, x=-0.1),
     )
     st.plotly_chart(fig,use_container_width=True)
 
 @st.cache_data
 def Monthly_count_bar_graph(successful_data_v):
-    successful_data_v['YearMonth'] = successful_data_v['PaymentDate'].dt.to_period('M')
+    successful_data_v['YearMonth'] = successful_data_v[PAYMENT_DATE_COLUMN].dt.to_period('M')
     monthly_counts = successful_data_v['YearMonth'].value_counts().sort_index()
 
     # Create a bar graph using Plotly with month names on the x-axis
@@ -135,10 +146,10 @@ def Filtered_data(successful_data_v):
     process_type_monthly_counts['ProcessType'] = process_type_monthly_counts['ProcessType_Description']
 
     # Group the data by 'ProcessType' and year-month of 'PaymentDate' and count the occurrences
-    process_type_monthly_counts = process_type_monthly_counts.groupby(['ProcessType', process_type_monthly_counts['PaymentDate'].dt.to_period('M')]).size().reset_index(name='Count')
+    process_type_monthly_counts = process_type_monthly_counts.groupby(['ProcessType', process_type_monthly_counts[PAYMENT_DATE_COLUMN].dt.to_period('M')]).size().reset_index(name='Count')
 
     # Format the 'PaymentDate' to 'YYYY-MM'
-    process_type_monthly_counts['PaymentDate'] = process_type_monthly_counts['PaymentDate'].dt.strftime('%Y-%m')
+    process_type_monthly_counts[PAYMENT_DATE_COLUMN] = process_type_monthly_counts[PAYMENT_DATE_COLUMN].dt.strftime('%Y-%m')
 
     all_descriptions = process_type_monthly_counts['ProcessType'].unique()
 
@@ -161,6 +172,7 @@ def PieChartProcessTypes(filtered_data):
     fig.update_traces(textposition='inside', textinfo='percent+label')
     fig.update_layout(
         title=f'Percent of each Process Type Contribution to Total Transaction Count',
+        legend=dict(orientation='v', y=1, x=-0.1),
     )
     st.plotly_chart(fig,use_container_width=True)
 
@@ -168,17 +180,18 @@ def PieChartProcessTypes(filtered_data):
 def ProcessTypeLineGraph(filtered_data):
     fig = px.line(
         filtered_data,
-        x='PaymentDate',
+        x=PAYMENT_DATE_COLUMN,
         y='Count',
         color='ProcessType',
         line_group='ProcessType',
-        labels={'PaymentDate': 'Month'},
+        labels={PAYMENT_DATE_COLUMN: 'Month'},
         color_discrete_sequence=px.colors.sequential.Aggrnyl
     )
     fig.update_layout(
         title=f'Total Count of Selected Process Type Descriptions Over Each Month',
         xaxis_title='Month',
-        yaxis_title='Count'
+        yaxis_title='Count',
+        legend=dict(orientation='h', y=-0.2, x=-0.1),
     )
     
     st.plotly_chart(fig,use_container_width=True)
@@ -186,7 +199,7 @@ def ProcessTypeLineGraph(filtered_data):
 
 @st.cache_data
 def Grouped_data_dayOfWeek(successful_data_v):
-    week_day_name_count = successful_data_v.groupby(['ProcessType_Description', successful_data_v['PaymentDate'].dt.day_name()]).size().reset_index(name='Count')
+    week_day_name_count = successful_data_v.groupby(['ProcessType_Description', successful_data_v[PAYMENT_DATE_COLUMN].dt.day_name()]).size().reset_index(name='Count')
 
     return week_day_name_count
 
@@ -195,11 +208,11 @@ def Grouped_data_dayOfWeek(successful_data_v):
 def TopDayOfWeekPlot(grouped_data,ordered_days):
     fig = px.bar(
         grouped_data, 
-        x='PaymentDate', 
+        x=PAYMENT_DATE_COLUMN, 
         y='Count', 
         color='ProcessType_Description', 
         labels={'Count': 'Total Count'},
-        category_orders={'PaymentDate': ordered_days} ,
+        category_orders={PAYMENT_DATE_COLUMN: ordered_days} ,
         color_discrete_sequence=px.colors.sequential.Aggrnyl,
         )
 
@@ -207,8 +220,10 @@ def TopDayOfWeekPlot(grouped_data,ordered_days):
     fig.update_layout(
         title='Total Count of Each ProcessType Description Grouped by Day of the Week',
         xaxis_title='Day of the Week',
-        yaxis_title='Total Count'
+        yaxis_title='Total Count',
+        legend=dict(orientation='h', y=-0.2, x=-0.1),
     )
+    
 
     st.plotly_chart(fig, use_container_width=True)
 
@@ -218,36 +233,64 @@ if csv_file is not None:
     data = load_data(csv_file)
     data_load_state.text("Done! (Loaded Successfully)")
 
-    selected_year = st.sidebar.selectbox("Select a Year", sorted(data[PAYMENT_DATE_COLUMN].dt.year.unique()))
-    data = selectDataYear(selected_year, data)
+    clean_data = processed_data(data)
 
-    processed_data_v = processed_data(data)
-    successful_data_v = successfulTransaction(processed_data_v)
-    failed_data_v = failedTransactions(processed_data_v)
+    year_options = sorted(clean_data[PAYMENT_DATE_COLUMN].dt.year.unique())
+    default_year = 2022
 
-    total_amount_v = successful_data_v['Amount'].sum()
+    selected_year = st.sidebar.selectbox("Select a Year", year_options, index=year_options.index(default_year))
+
+
+    processed_data_v = selectDataYear(selected_year, clean_data)
+
     Total_v = len(processed_data_v)
+    total_amount_v = processed_data_v['Amount'].sum()
+    users_v = processed_data_v['CompanyName'].str.strip().nunique()
+
+
+
+    successful_data_v = successfulTransaction(processed_data_v)
+
+    declined = declinedTransactions(processed_data_v)
+    not_declined = notDeclined(processed_data_v)
+
+
+   
     success_v = len(successful_data_v)
-    failed_v = len(failed_data_v)
+   
 
-    users_v = data['CompanyName'].nunique()
-    Transaction_types_v = data['ProcessType'].nunique()
+    not_declined_v = len(not_declined)
+    declined_v = len(declined)
 
-    success_percent = str(round(success_v / Total_v * 100, 2))
-    failed_v_percent = str(round(failed_v / Total_v * 100, 2))
+    un_successful_v = not_declined_v - success_v
+
+    
+    Transaction_types_v = processed_data_v['ProcessType'].nunique()
+
+
+
+    undeclined_percent = str(round(not_declined_v / Total_v * 100, 2))
+    declined_v_percent = str(round(declined_v / Total_v * 100, 2))
 
     with st.container():
+
+        st.title(f"Analysing GAPS Transaction History In {selected_year}")
+       
+        st.markdown("---")
+        st.subheader("Metrics")
         col1, col2, col3, col4, col5 = st.columns(5)
 
         with col1:
-            st.metric(label="Total Count", value=str("{:,.0f}".format(Total_v)))
-            st.metric(label="Total Amount", value=str(humanize.intword(total_amount_v)))
+            st.metric(label=f"Total Count", value=str("{:,.0f}".format(Total_v)))
+            st.metric(label=f"Total Amount", value=str(humanize.intword(total_amount_v)))
         with col2:
-            st.metric(label=f"Successful Transaction Count", value=str("{:,.0f}".format(success_v)), delta = f"+ Success  - ({success_percent} %)" )
+            st.metric(label=f"UnDeclined Transaction Count ", value=str("{:,.0f}".format(not_declined_v)), delta = f"+ UnDeclined  - ({undeclined_percent} %)" )
+            st.metric(label=f"Processed Transactions ", value=str("{:,.0f}".format(success_v)))
+            st.metric(label=f"Un-Processed Transactions ", value=str("{:,.0f}".format(un_successful_v)))
         with col3:
-            st.metric(label=f"Failed Transactions Count", value=str( "{:,.0f}".format(failed_v)) , delta = f"- Failed - ({failed_v_percent} %)" )
+            st.metric(label=f"Declined Transactions Count ", value=str( "{:,.0f}".format(declined_v)) , delta = f"- Declined - ({declined_v_percent} %)" )
         with col4:
-            st.metric(label="Total Users", value=str(users_v) )
+            st.metric(label=f"Total Companies", value=str(users_v) )
         with col5:
             st.metric(label="Transaction Types", value=str(Transaction_types_v))
      
@@ -260,13 +303,10 @@ if csv_file is not None:
         st.subheader('Quarterly Transaction Summary')
         col3,col1, col2 = st.columns([1,2,2])
 
-            
-
         # Resample the data to quarterly counts
-        quarterly_counts = successful_data_v.resample('Q', on='PaymentDate').size()
-
+        quarterly_counts = successful_data_v.resample('Q', on=PAYMENT_DATE_COLUMN).size()    
         # Calculate the total amount per quarter
-        quarterly_total_amount = successful_data_v.resample('Q', on='PaymentDate')['Amount'].sum()
+        quarterly_total_amount = successful_data_v.resample('Q', on=PAYMENT_DATE_COLUMN)['Amount'].sum()
 
         # Get the labels for quarters
         quarter_labels = ['Q1', 'Q2', 'Q3', 'Q4']
@@ -338,21 +378,18 @@ if csv_file is not None:
 
     with st.container():
         st.subheader("Transaction Data Tables")
-        tab1, tab2, tab3, tab4 = st.tabs([f"Raw Data ({Total_v})", f"Processed ({Total_v})", f"Successful Transactions ({success_v})", f"Failed Transactions ({failed_v})"])
+        tab1, tab2, tab3 = st.tabs([f"Raw Data ({len(data)})", f"Processed Transactions ({success_v})", f"Declined Transactions ({len(declined)})"])
         with tab1:
-            st.write('**Raw data**')
+            st.write('**Raw data ( Year: Mixed)**')
             st.write(data)
 
         with tab2:
-            st.write('**Processed data**')
-            st.write(processed_data_v)
-
-        with tab3:
-            st.write('**Successful Transactions**')
+            st.write(f'**Processed data (No: {len(successful_data_v)})**')
             st.write(successful_data_v)
 
-        with tab4:
-            st.write('**Failed Transactions**')
-            st.write(failed_data_v)
+        with tab3:
+            st.write(f'**Declined Transactions (No: {len(declined)})**')
+            st.write(declined)
+
 
         st.markdown("---")
